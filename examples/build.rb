db599b7b94e9b08159c577d4f52fa9923ad049e0
Richard Mavis <rmavis@gmail.com>
2018-12-14 19:05:29 -0800

    Add more files to the examples.

---
+++ examples/build.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+require_relative '_autoload.rb'
+
+class Build
+
+  # Build.run :: [string] -> void
+  def self.run(args)
+    if (self.respond_to?(args[0]))
+      self.send(args[0], args.slice(1, (args.length - 1)))
+    else
+      $stderr.puts "Can't build: `#{args[0]}` doesn't name a build method."
+    end
+  end
+
+
+  #
+  # Runnable functions / valid arguments.
+  #
+
+  # Build.mdposts :: [string] -> void
+  def self.mdposts(args)
+    dir = "#{DirMap.content}/posts"
+    Dir.foreach(dir) do |entry|
+      file = "#{dir}/#{entry}"
+      if (self.is_readable_file?(file))
+        system("./cms.rb view html -s MarkdownArticle #{args.join(' ')} #{file}")
+      else
+        $stderr.puts "Skipping `#{file}`."
+      end
+    end
+  end
+
+  def self.content(args)
+    Dir.foreach(DirMap.content) do |entry|
+      file = "#{DirMap.content}/#{entry}"
+      if ((self.is_readable_file?(file)) &&
+          (File.extname(file) == ".yaml"))
+        system("./cms.rb view html #{args.join(' ')} #{file}")
+      else
+        $stderr.puts "Skipping `#{file}`."
+      end
+    end
+  end
+
+
+  #
+  # Utility functions.
+  #
+
+  def self.is_readable_file?(path)
+    ((!File.directory?(path)) &&
+     (File.readable?(path)) &&
+     (path != '..') &&
+     (path != '.'))
+  end
+
+end
+
+Build.run(ARGV)

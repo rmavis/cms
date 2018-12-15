64b245630e1090b304961e25205a4c6031d57b33
Richard Mavis <rmavis@gmail.com>
2018-12-14 20:51:27 -0800

    Revise the initial autoload.

--- _autoload.rb
+++
@@ -1,2 +0,0 @@
-require_relative 'base/_autoload.rb'
-require_relative 'examples/_autoload.rb'
--- cms.rb
+++ cms.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/env ruby
 
-require_relative '_autoload.rb'
+# This file loaded by this line should load the module that contains
+# the local Specs. It is needed!
+require_relative 'local/_autoload.rb'
+
+# The file loaded by this line loads the module that contains the
+# base classes. It is also needed.
+require_relative 'base/_autoload.rb'
+
+# And this runs the given command.
 Base::CLI.parse_args(ARGV)
